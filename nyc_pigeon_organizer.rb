dd13c773a7ce2ccf965e354787b430dd0bd5e183
Toby Boudreaux <toby.boudreaux11@gmail.com>
2020-05-09 15:25:03 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -7,7 +7,7 @@ def nyc_pigeon_organizer(data)
         if !new_hash.has_key?(name)
           new_hash[name] = {}
         end 
-        if !new_hash.has_key(property)
+        if !new_hash.has_key?(property)
           new_hash[name][property] = []
         end 
     
