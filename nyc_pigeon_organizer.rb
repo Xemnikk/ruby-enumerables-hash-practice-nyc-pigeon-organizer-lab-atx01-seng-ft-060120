b181751efab9e3f0f17f5d2f554871ca1028d225
Toby Boudreaux <toby.boudreaux11@gmail.com>
2020-05-09 15:42:16 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -4,13 +4,13 @@ def nyc_pigeon_organizer(data)
   data.each do |property, hash|
     hash.each do |attribute, array|
       array.each do |name|
-        if new_hash.has_key?(name)
+        if !new_hash.has_key?(name)
           new_hash[name] = {}
         end 
-        if new_hash[name].has_key?(property)
+        if !new_hash[name].has_key?(property)
           new_hash[name][property] = []
         end 
-        if new_hash[name][property].include?(attribute)
+        if !new_hash[name][property].include?(attribute)
           new_hash[name][property] << attribute.to_s
         end
       end 
