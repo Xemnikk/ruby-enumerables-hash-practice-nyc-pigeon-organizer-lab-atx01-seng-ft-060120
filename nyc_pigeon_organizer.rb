d31e28eeb6b7bfdd19f1a629a93d7298e212262c
Toby Boudreaux <toby.boudreaux11@gmail.com>
2020-05-09 15:23:02 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -4,7 +4,7 @@ def nyc_pigeon_organizer(data)
   data.each do |property, hash|
     hash.each do |attribute, array|
       array.each do |name|
-        if !new_hash
+        if !new_hash.has_key?(name)
     
   
   puts data
